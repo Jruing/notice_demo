f5f1258e2f43129367d86d6b4236801b739d9282
姚佳瑞 <jruing@126.com>
2024-07-05 22:16:22 +0800

    优化用户接口

--- api/user.go
+++ api/user.go
@@ -34,20 +34,19 @@ func UserAdd(c *gin.Context) {
 	user.UserName = json["username"].(string)
 	user.PassWord = json["passwd"].(string)
 	user.Remarks = json["remarks"].(string)
-	user.Id = int64(json["id"].(float64))
 	user.UserType = int64(json["usertype"].(float64))
 	_, err = session.Insert(user)
 	if err != nil {
-		logger.Error("用户添加失败", zap.String("uuid", log_uuid.String()), zap.String("error", err.Error()))
+		logger.Error("用户新增失败", zap.String("uuid", log_uuid.String()), zap.String("error", err.Error()))
 		c.JSON(200, gin.H{
 			"Code": 0,
-			"Msg":  "用户添加失败",
+			"Msg":  "用户新增失败",
 		})
 	} else {
-		logger.Info("用户添加成功", zap.String("uuid", log_uuid.String()))
+		logger.Info("用户新增成功", zap.String("uuid", log_uuid.String()))
 		c.JSON(200, gin.H{
 			"Code": 1,
-			"Msg":  "用户添加成功",
+			"Msg":  "用户新增成功",
 		})
 	}
 
@@ -73,16 +72,16 @@ func UserDelete(c *gin.Context) {
 	user := new(models.User)
 	_, err = session.Where("id = ?", id).Delete(user)
 	if err != nil {
-		logger.Error("删除失败", zap.String("uuid", log_uuid.String()), zap.String("error", err.Error()))
+		logger.Error("用户删除失败", zap.String("uuid", log_uuid.String()), zap.String("error", err.Error()))
 		c.JSON(200, gin.H{
 			"Code": 0,
-			"Msg":  "删除用户失败",
+			"Msg":  "用户删除失败",
 		})
 	} else {
-		logger.Info("删除用户成功", zap.String("uuid", log_uuid.String()))
+		logger.Info("用户删除成功", zap.String("uuid", log_uuid.String()))
 		c.JSON(200, gin.H{
 			"Code": 1,
-			"Msg":  "删除用户成功",
+			"Msg":  "用户删除成功",
 		})
 	}
 }
@@ -118,13 +117,13 @@ func UserQuery(c *gin.Context) {
 		logger.Error("用户查询失败", zap.String("error", err.Error()))
 		c.JSON(200, gin.H{
 			"Code": -2,
-			"Msg":  "用户数据查询失败",
+			"Msg":  "用户查询失败",
 		})
 	} else {
 		logger.Info("用户查询成功", zap.String("uuid", log_uuid.String()))
 		c.JSON(200, gin.H{
 			"Code": 200,
-			"Msg":  "用户数据查询成功",
+			"Msg":  "用户查询成功",
 			"Data": user,
 		})
 	}
@@ -150,20 +149,19 @@ func UserUpdate(c *gin.Context) {
 	user.UserName = json["username"].(string)
 	user.PassWord = json["passwd"].(string)
 	user.Remarks = json["remarks"].(string)
-	user.Id = int64(json["id"].(float64))
 	user.UserType = int64(json["usertype"].(float64))
 	_, err = session.Where("id = ?", user.Id).Update(user)
 	if err != nil {
 		logger.Error("用户修改失败", zap.String("uuid", log_uuid.String()), zap.String("error", err.Error()))
 		c.JSON(200, gin.H{
 			"Code": -2,
-			"Msg":  "修改用户失败",
+			"Msg":  "用户修改失败",
 		})
 	} else {
 		logger.Info("用户修改成功", zap.String("uuid", log_uuid.String()))
 		c.JSON(200, gin.H{
 			"Code": 200,
-			"Msg":  "修改用户成功",
+			"Msg":  "用户修改成功",
 		})
 	}
 }
